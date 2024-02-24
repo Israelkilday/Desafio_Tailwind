1e1e7a6f496c791377387d9e1b2e6d2bed33f9f1
Israel Kilday <israel.kilday@yahoo.com.br>
2024-02-23 22:44:03 -0300

    feat: applying zoon efect on card tenis

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,17 @@ module.exports = {
         "sans": ["Poppins"]
       },
       backgroundImage: {
-        "bg_card_tenis": "url('/src/assets/bg.png')"
+        "bg_card_tenis": "url('../src/assets/bg.png')"
+      },
+      keyframes: {
+        floatRight: {
+          "0%": { transform: "translatey(0px)" },
+          "50%": { transform: "translatey(8px)" },
+          "100%": { transform: "translatey(0px)" },
+        }
+      },
+      animation: {
+        floatRight: "floatRight 2.5s infinite"
       }
     },
   },
